c67fb17d3be16df464d00cce1b3042187d0c6d92
Sarfraj <breezy.safu@gmail.com>
2018-12-25 18:39:49 +0545

    3

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -8,11 +8,19 @@
           href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
           integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
           crossorigin="anonymous">
+    <link rel="stylesheet"
+          href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
+          integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/"
+          crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ URL::to('src/css/app.css') }}">
 
     @yield('styles')
 </head>
 <body>
-@yield('content')
+@include('partials.header')
+<div class="container">
+    @yield('content')
+</div>
 
 <script
         src="https://code.jquery.com/jquery-3.3.1.min.js"
