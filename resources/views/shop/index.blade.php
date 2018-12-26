01e97a3e1236db96aa6e7ad7d941b0e67f6013e7
Sarfraj <breezy.safu@gmail.com>
2018-12-26 17:06:43 +0545

    5

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -5,128 +5,24 @@
 @endsection
 
 @section('content')
-    <div class="row">
-        <div class="col-sm-6 col-md-4">
-            <div class="thumbnail">
-                <img src="http://img.timeinc.net/time/2007/top_10_photos/fiction_harry_potter_a.jpg"
-                     alt="..." class="img-responsive">
-                <div class="caption">
-                    <h3>Product Title</h3>
-                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        d do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-                        dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                        mollit anim id est laborum</p>
-                    <div class="clearfix">
-                        <div class="pull-left price">$12</div>
-                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+    @foreach($products->chunk(3) as $productChunk)
+        <div class="row">
+            @foreach($productChunk as $product)
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+                        <img src="{{ $product->imagePath }}"
+                             alt="..." class="img-responsive">
+                        <div class="caption">
+                            <h3>{{ $product->title }}</h3>
+                            <p class="description">{{ $product->description }}</p>
+                            <div class="clearfix">
+                                <div class="pull-left price">{{ $product->price }}</div>
+                                <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
+            @endforeach
         </div>
-        <div class="col-sm-6 col-md-4">
-            <div class="thumbnail">
-                <img src="http://img.timeinc.net/time/2007/top_10_photos/fiction_harry_potter_a.jpg"
-                     alt="..." class="img-responsive">
-                <div class="caption">
-                    <h3>Product Title</h3>
-                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        d do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-                        dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                        mollit anim id est laborum</p>
-                    <div class="clearfix">
-                        <div class="pull-left price">$12</div>
-                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-6 col-md-4">
-            <div class="thumbnail">
-                <img src="http://img.timeinc.net/time/2007/top_10_photos/fiction_harry_potter_a.jpg"
-                     alt="..." class="img-responsive">
-                <div class="caption">
-                    <h3>Product Title</h3>
-                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        d do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-                        dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                        mollit anim id est laborum</p>
-                    <div class="clearfix">
-                        <div class="pull-left price">$12</div>
-                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-sm-6 col-md-4">
-            <div class="thumbnail">
-                <img src="http://img.timeinc.net/time/2007/top_10_photos/fiction_harry_potter_a.jpg"
-                     alt="..." class="img-responsive">
-                <div class="caption">
-                    <h3>Product Title</h3>
-                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        d do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-                        dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                        mollit anim id est laborum</p>
-                    <div class="clearfix">
-                        <div class="pull-left price">$12</div>
-                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-6 col-md-4">
-            <div class="thumbnail">
-                <img src="http://img.timeinc.net/time/2007/top_10_photos/fiction_harry_potter_a.jpg"
-                     alt="..." class="img-responsive">
-                <div class="caption">
-                    <h3>Product Title</h3>
-                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        d do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-                        dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                        mollit anim id est laborum</p>
-                    <div class="clearfix">
-                        <div class="pull-left price">$12</div>
-                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-6 col-md-4">
-            <div class="thumbnail">
-                <img src="http://img.timeinc.net/time/2007/top_10_photos/fiction_harry_potter_a.jpg"
-                     alt="..." class="img-responsive">
-                <div class="caption">
-                    <h3>Product Title</h3>
-                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        d do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-                        dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-                        mollit anim id est laborum</p>
-                    <div class="clearfix">
-                        <div class="pull-left price">$12</div>
-                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    @endforeach
 @endsection
